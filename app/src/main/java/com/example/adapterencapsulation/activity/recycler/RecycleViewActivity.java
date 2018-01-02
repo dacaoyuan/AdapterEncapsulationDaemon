3722894d10cfc1bd4ea329250b832d478943838f
YuanPeikai <2908363622@qq.com>
2018-01-02 15:09:27 +0800

    添加了RecycleView滚动监听的使用

--- app/src/main/java/com/example/adapterencapsulation/activity/recycler/RecycleViewActivity.java
+++ app/src/main/java/com/example/adapterencapsulation/activity/recycler/RecycleViewActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -20,7 +21,7 @@ import butterknife.ButterKnife;
  * RecycleView 的基本用法
  */
 public class RecycleViewActivity extends AppCompatActivity {
-
+    private static final String TAG = "RecycleViewActivity";
     @BindView(R.id.recy)
     RecyclerView recyclerView;
 
@@ -46,6 +47,31 @@ public class RecycleViewActivity extends AppCompatActivity {
             @Override
             public void onItemLongClick(View view, int position) {
 
+            }
+        });
+
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                Log.i(TAG, "onScrollStateChanged: newState=" + newState);
+                switch (newState) {
+                    case RecyclerView.SCROLL_STATE_DRAGGING:
+                        Log.i(TAG, "onScrollStateChanged: 滚动状态拖");
+                        break;
+                    case RecyclerView.SCROLL_STATE_IDLE:
+                        Log.i(TAG, "onScrollStateChanged: 滚动状态闲置");
+                        break;
+                    case RecyclerView.SCROLL_STATE_SETTLING:
+                        Log.i(TAG, "onScrollStateChanged: 滚动状态解决");
+                        break;
+                }
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+               Log.i(TAG, "onScrolled: dx=" + dx + " dy=" + dy);
             }
         });
 
